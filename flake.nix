070037a08310e93742a55f7ff3a00d5176c56994
Daniil (fakelog) <48557356+fakelog@users.noreply.github.com>
2024-07-24 00:34:39 +0500

    flake: init

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  description = "Fluent icon theme for linux desktops";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachSystem [ "x86_64-linux" "aarch64-linux" ] (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        packages.default = pkgs.callPackage ./package.nix {};
+      }
+    );
+}
---
+++ package.nix
@@ -0,0 +1,58 @@
+{ lib,
+  stdenvNoCC,
+  fetchFromGitHub,
+  gtk3,
+  hicolor-icon-theme,
+  jdupes,
+  name ? "Fluent",
+  colorVariants ? [ "standard" ]
+}:
+let
+  pname = "Fluent-icon-theme";
+in
+  lib.checkListOfEnum "${pname}: available color variants" [ "standard" "green" "grey" "orange" "pink" "purple" "red" "yellow" "teal" ] colorVariants
+  stdenvNoCC.mkDerivation
+rec {
+  inherit pname;
+  version = "master";
+
+  src = fetchFromGitHub {
+    owner = "vinceliuice";
+    repo = pname;
+    rev = version;
+    hash = "sha256-6VPuqvXqtoygBnZ7u/aRqV0jCvjjWLSqyMIXkkF7YOc=";
+  };
+
+  nativeBuildInputs = [ gtk3 jdupes ];
+
+  buildInputs = [ hicolor-icon-theme ];
+
+  # Unnecessary & slow fixup's
+  dontPatchELF = true;
+  dontRewriteSymlinks = true;
+  dontDropIconThemeCache = true;
+
+  postPatch = ''
+    patchShebangs install.sh
+  '';
+
+  installPhase = ''
+    runHook preInstall
+
+    ./install.sh --dest $out/share/icons \
+      --name ${name} \
+      ${builtins.toString colorVariants}
+
+    jdupes --quiet --link-soft --recurse $out/share
+
+    runHook postInstall
+  '';
+
+  meta = with lib; {
+    description = "Fluent icon theme for linux desktops";
+    homepage = "https://github.com/vinceliuice/Fluent-icon-theme";
+    license = licenses.gpl3Plus;
+    maintainers = with maintainers; [ fakelog ];
+    platforms = platforms.linux;
+  };
+}
